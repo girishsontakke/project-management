f04f4454b95f5f1286987291a16a07731a657dfc
girishsontakke <girishsontakke7@gmail.com>
2021-04-29 21:54:38 +0530

    :heavy_check_mark: deploy the app with gh-pages

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const CleanPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "production",
@@ -9,10 +10,19 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
   module: {
-    rules: [{ test: /\.ts$/, use: "ts-loader", exclude: /node_modules/ }],
+    rules: [
+      { test: /\.ts$/, use: "ts-loader", exclude: /node_modules/ },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
   resolve: {
     extensions: [".ts", ".js"],
   },
-  plugins: [new CleanPlugin.CleanWebpackPlugin()],
+  plugins: [
+    new CleanPlugin.CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ template: "./index.html" }),
+  ],
 };
